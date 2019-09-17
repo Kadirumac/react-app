18e275878a72bccd82f52d07c5a94fc8e9488d15
kadirumac <kadirumac49@gmail.com>
2019-09-17 10:57:15 +0200

    Updates

---
+++ precache-manifest.ece94a597e8af9ecaf7367a942687efc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea2dd285420495397ed34ce154a44640",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "2f3421a1e2c643f3f173",
+    "url": "/react-app/static/css/main.099eedfb.chunk.css"
+  },
+  {
+    "revision": "a1b43cd7f96d1333a078",
+    "url": "/react-app/static/js/2.40ec26ae.chunk.js"
+  },
+  {
+    "revision": "2f3421a1e2c643f3f173",
+    "url": "/react-app/static/js/main.cf7d5bfb.chunk.js"
+  },
+  {
+    "revision": "8a4c3404cc3a6a9e68d6",
+    "url": "/react-app/static/js/runtime~main.6282128a.js"
+  }
+]);
